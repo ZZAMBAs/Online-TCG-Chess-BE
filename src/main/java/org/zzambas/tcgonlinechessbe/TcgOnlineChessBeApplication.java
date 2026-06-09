7da94e75169298a082dc0827a453b4f8712677e3
ZZAMBAs <rksidksrksi@naver.com>
2026-06-09 23:08:57 +0900

    initial commit

---
+++ src/main/java/org/zzambas/tcgonlinechessbe/TcgOnlineChessBeApplication.java
@@ -0,0 +1,13 @@
+package org.zzambas.tcgonlinechessbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgOnlineChessBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcgOnlineChessBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zzambas/tcgonlinechessbe/TcgOnlineChessBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zzambas.tcgonlinechessbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgOnlineChessBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
